8298d5e244389090a2bd10aeacb7ea5cc61058c7
Mahmudul Karim Lincoln <74609643+lincoln4791@users.noreply.github.com>
2024-12-08 15:45:37 +0600

    Compose Integrated

--- app/src/main/java/com/lincoln4791/goldcalculatorbd/admobAdsUpdated/AdUnitIds.kt
+++ app/src/main/java/com/lincoln4791/goldcalculatorbd/admobAdsUpdated/AdUnitIds.kt
@@ -4,6 +4,7 @@ class AdUnitIds {
     companion object{
 
         const val INTERSTITIAL= "ca-app-pub-5778949627900306/4282132698"
+        const val APP_OPEN= "ca-app-pub-5778949627900306/8602648476"
 
     }
 }
